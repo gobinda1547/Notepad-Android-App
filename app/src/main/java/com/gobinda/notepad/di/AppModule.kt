55041f54226478b8c7b4a797c3551ee81930cabe
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-03-15 02:47:07 +0600

    Providing note database object, note dao object and note repository object via Hilt.

---
+++ app/src/main/java/com/gobinda/notepad/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.gobinda.notepad.di
+
+import android.content.Context
+import androidx.room.Room
+import com.gobinda.notepad.data.repository.NoteRepository
+import com.gobinda.notepad.data.repository.NoteRepositoryImpl
+import com.gobinda.notepad.data.source.NoteDao
+import com.gobinda.notepad.data.source.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(@ApplicationContext appContext: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context = appContext,
+            klass = NoteDatabase::class.java,
+            name = NoteDatabase.DB_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao {
+        return noteDatabase.noteDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(noteDao: NoteDao): NoteRepository {
+        return NoteRepositoryImpl(noteDao)
+    }
+}
