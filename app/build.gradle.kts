2e7d8909eb316de936b2dc47b1b7995522445b09
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-03-14 23:11:33 +0600

    Adding room database dependency in build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,10 @@ dependencies {
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
 
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
