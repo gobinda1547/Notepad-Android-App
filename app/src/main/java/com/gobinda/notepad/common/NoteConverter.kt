26dce48e30f33f69c69f802f43141045bb130b62
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-03-16 16:33:31 +0600

    Implemented get single note use case with unit test

--- app/src/main/java/com/gobinda/notepad/common/NoteConverter.kt
+++ app/src/main/java/com/gobinda/notepad/common/NoteConverter.kt
@@ -10,3 +10,11 @@ import com.gobinda.notepad.domain.model.Note
 fun Note.toNoteModel(): NoteModel {
     return NoteModel(id, title, content, lastEditTime)
 }
+
+/**
+ * This is an extension function written on behave of [NoteModel] class.
+ * This function basically converted [NoteModel] object to [Note] object.
+ */
+fun NoteModel.toNote(): Note {
+    return Note(id, title, content, lastEditTime)
+}
---
+++ app/src/test/java/com/gobinda/notepad/domain/usecase/GetSingleNoteUseCaseTest.kt
@@ -0,0 +1,51 @@
+package com.gobinda.notepad.domain.usecase
+
+import com.gobinda.notepad.data.model.NoteModel
+import com.gobinda.notepad.data.repository.NoteRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetSingleNoteUseCaseTest {
+
+    private lateinit var repository: NoteRepository
+    private lateinit var useCase: GetSingleNoteUseCase
+
+    @Before
+    fun setup() {
+        repository = mockk()
+        useCase = GetSingleNoteUseCase(repository)
+    }
+
+    @Test
+    fun getSingleNoteWhenTheListIsEmpty() = runBlocking {
+        val testNoteId = 5L
+        coEvery { repository.getSingleNote(testNoteId) } returns flowOf(null)
+
+        val singleNote = useCase.execute(testNoteId).first()
+        TestCase.assertEquals(null, singleNote)
+    }
+
+    @Test
+    fun getSingleNoteWhenTheListIsNotEmpty() = runBlocking {
+        val testNoteId = 5L
+        val testNote = NoteModel(
+            id = testNoteId,
+            title = "test title",
+            content = "test content",
+            lastEditTime = 100L
+        )
+        coEvery { repository.getSingleNote(testNoteId) } returns flowOf(testNote)
+
+        val singleNote = useCase.execute(testNoteId).first()
+        TestCase.assertEquals(testNote.id, singleNote?.id)
+        TestCase.assertEquals(testNote.title, singleNote?.title)
+        TestCase.assertEquals(testNote.content, singleNote?.content)
+        TestCase.assertEquals(testNote.lastEditTime, singleNote?.lastEditTime)
+    }
+}
