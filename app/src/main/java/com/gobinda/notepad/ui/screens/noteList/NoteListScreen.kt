35256b0bbde5d26a924cddf7e0f807c395aa876c
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-03-16 20:21:57 +0600

    UI side implementation for note list screen is done.

--- app/src/main/java/com/gobinda/notepad/ui/screens/noteList/NoteListScreen.kt
+++ app/src/main/java/com/gobinda/notepad/ui/screens/noteList/NoteListScreen.kt
@@ -1,13 +1,70 @@
 package com.gobinda.notepad.ui.screens.noteList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.gobinda.notepad.domain.model.NoteAsListItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteListScreen(
     navController: NavController,
     viewModel: NoteListViewModel = hiltViewModel()
 ) {
+    val noteItems: State<List<NoteAsListItem>?> = viewModel.noteList.collectAsState()
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground
+                ),
+                title = { Text(text = "Notepad") },
+                actions = {
+                    IconButton(onClick = { /* todo - have to implement */ }) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(innerPadding)
+        ) {
+            noteItems.value?.let { validItems ->
+                when (validItems.isEmpty()) {
+                    true -> NoteListEmptyView {
+                        // todo - since empty view clicked so we have to take
+                        // todo - the user to add new note screen
+                    }
+
+                    else -> NoteListNonEmptyValue(noteItems = validItems) {
+                        // todo - since non empty view clicked so we have to take
+                        // todo - the user to single note screen by using the id
+                    }
+                }
+            }
+        }
+    }
 }
