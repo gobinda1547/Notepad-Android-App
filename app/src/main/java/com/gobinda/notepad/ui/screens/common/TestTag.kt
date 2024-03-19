733d3e41f60b86387efeb9ae8add63a9743f1b72
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-03-20 03:20:13 +0600

    Writing instrumentation test for NoteListEmptyView

--- app/src/main/java/com/gobinda/notepad/ui/screens/common/TestTag.kt
+++ app/src/main/java/com/gobinda/notepad/ui/screens/common/TestTag.kt
@@ -6,4 +6,5 @@ object TestTag {
     const val TitleInputView_InputField = "TitleInputView_input_field"
     const val TitleInputView_HintText = "TitleInputView_hint_text"
     const val MenuDivider_Main = "MenuDivider_main_inner_divider"
+    const val NoteListEmptyView_Main_Text = "NoteListEmptyView_main_text"
 }
