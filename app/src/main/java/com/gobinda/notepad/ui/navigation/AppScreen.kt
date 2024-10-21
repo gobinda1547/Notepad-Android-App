6c4b55742afaafd44f7ce9eb98e85ded54983d4c
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-10-21 01:32:29 -0600

    Handling compose navigation in a more easiest way

--- app/src/main/java/com/gobinda/notepad/ui/navigation/AppScreen.kt
+++ app/src/main/java/com/gobinda/notepad/ui/navigation/AppScreen.kt
@@ -1,7 +1,37 @@
 package com.gobinda.notepad.ui.navigation
 
-sealed class AppScreen(val router: String) {
-    data object NoteListScreen : AppScreen("note_list_screen")
-    data object AddOrEditNoteScreen : AppScreen("add_edit_note_screen")
-    data object ShowNoteScreen : AppScreen("show_note_screen")
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+sealed class AppScreen(
+    val route: String,
+    val navArguments: List<NamedNavArgument> = emptyList()
+) {
+
+    data object NoteListScreen : AppScreen(
+        route = "note_list_screen"
+    )
+
+    data object AddOrEditNoteScreen : AppScreen(
+        route = "add_edit_note_screen/{noteId}",
+        navArguments = listOf(
+            navArgument(name = "noteId") {
+                type = NavType.LongType
+            }
+        )
+    ) {
+        fun createRoute(noteId: Long) = "add_edit_note_screen/${noteId}"
+    }
+
+    data object ShowNoteScreen : AppScreen(
+        route = "show_note_screen/{noteId}",
+        navArguments = listOf(
+            navArgument(name = "noteId") {
+                type = NavType.LongType
+            }
+        )
+    ) {
+        fun createRoute(noteId: Long) = "show_note_screen/${noteId}"
+    }
 }
